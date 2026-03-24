38ead6e65192d71aabbe311012f09dd92556f121
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-24 21:11:52 +0200

    feat: reading all model files

---
+++ backend/models/index.js
@@ -0,0 +1,38 @@
+// models/index.js
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const basename = path.basename(__filename);
+const db = {};
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT,
+    logging: false
+  }
+);
+
+// Read all model files
+fs.readdirSync(__dirname)
+  .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+// Apply associations
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) db[modelName].associate(db);
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
