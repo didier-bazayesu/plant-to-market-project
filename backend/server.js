55ce38fcdbb0e35d8d14a60461f4c46d37c5972c
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-24 22:40:13 +0200

    feat: running server

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./models');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Test route
+app.get('/', (req, res) => res.send('Plant-to-Market API is running'));
+
+// Routes
+app.use('/farmers', require('./routes/farmerRoutes'));
+app.use('/farms', require('./routes/farmRoutes'));
+app.use('/crops', require('./routes/cropRoutes'));
+app.use('/activities', require('./routes/activityRoutes'));
+app.use('/harvests', require('./routes/harvestRoutes'));
+app.use('/marketprices', require('./routes/marketPriceRoutes'));
+
+// Start server
+app.listen(PORT, async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log(`Database connected and server running on http://localhost:${PORT}`);
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+  }
+});
