fc7acb713ec633375adc10c2290b7da35dc76887
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-24 21:10:12 +0200

    feat: creating activity table

---
+++ backend/models/Activity.js
@@ -0,0 +1,16 @@
+// models/Activity.js
+module.exports = (sequelize, DataTypes) => {
+  const Activity = sequelize.define('Activity', {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    crop_id: { type: DataTypes.INTEGER, allowNull: false },
+    type: { type: DataTypes.ENUM('irrigation','fertilization','pesticide'), allowNull: false },
+    date: { type: DataTypes.DATE, allowNull: false },
+    notes: { type: DataTypes.TEXT }
+  }, { timestamps: false });
+
+  Activity.associate = models => {
+    Activity.belongsTo(models.Crop, { foreignKey: 'crop_id' });
+  };
+
+  return Activity;
+};
