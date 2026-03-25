9920d74b422858f1fa312b2addc3fd7f257b2fa3
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-25 09:43:08 +0200

    feat:  updating the routes to use validator function middleware

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const activityController = require('../controllers/activityController');
+const { createActivityValidation } = require('../validators/activityValidator');
+const validate = require('../middlewares/validate');
 
-router.post('/', activityController.createActivity);
+router.post('/', createActivityValidation, validate, activityController.createActivity);
 router.get('/', activityController.getActivities);
 
 module.exports = router;
