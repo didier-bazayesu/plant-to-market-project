91682c252f796850f67a1b39e5f895b2322268b3
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-26 13:19:54 +0200

    feat: render whole project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+
+import Layout from "./components/Layout";
+import HomeFarm from "./pages/HomeFarm";
+import Farmers from "./pages/Farmers";
+import Farm from "./pages/Farm";
+import Crops from "./pages/Crops";
+import About from "./pages/About";
+import Activity from "./pages/Activities";
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {/* Public routes */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomeFarm />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+
+      {/* Protected routes */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }
+      >
+        <Route path="farmers" element={<Farmers />} />
+        <Route path="activities" element={<Activity />} />
+        <Route path="farms" element={<Farm />} />
+        <Route path="crops" element={<Crops />} />
+      </Route>
+    </>
+  )
+);
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
