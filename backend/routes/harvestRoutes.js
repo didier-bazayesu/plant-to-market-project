ae54ac8e82c47fbdc3a7d9098ce61f5279ee424a
didier-bazayesu <didierbazayesu@gmail.com>
2026-05-14 21:28:42 +0200

    feat: adding the routes for harvest

--- backend/routes/harvestRoutes.js
+++ backend/routes/harvestRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const harvestController = require('../controllers/harvestController');
 const { createHarvestValidation } = require('../validators/harvestValidator');
 const validate = require('../middlewares/validate');
+const { protect } = require('../middlewares/auth');  // ← add this
 
-router.post('/', createHarvestValidation, validate, harvestController.createHarvest);
-router.get('/', harvestController.getHarvests);
+router.post('/', protect, createHarvestValidation, validate, harvestController.createHarvest);
+router.get('/', protect, harvestController.getHarvests);
 
 module.exports = router;
