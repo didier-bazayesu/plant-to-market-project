11dec0e333e411e9f39e547344cf6491c67cbaba
didier-bazayesu <didierbazayesu@gmail.com>
2026-05-23 11:48:08 +0200

    feat:  add comment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL , 'http://localhost:5173'],
+  origin: [process.env.FRONTEND_URL ,'http://localhost:5173'],
 }));
 app.use(bodyParser.json());
 
